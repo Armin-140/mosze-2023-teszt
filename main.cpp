a43efe1f1e74c9e77d9989173f73f19f0affd9f0
Armin-140 <kovacsarminkrisztian@gmail.com>
2026-02-18 18:35:57 +0100

    Hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibasan hivatkozas a integerre
+    std::cout << '1-100 ertekek duplazasa' //szimpla idezojel van megadva macskakorom helyett, nincs pontosvesszo
+    for (int i = 0;) //a forciklusnak hianyoznak ertekei
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hianyzik a pontosvesszo, emellett hianyzik az "i"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // sima vesszo van megadva pontosvesszo helyett
     {
-        atlag += b[i]
+        atlag += b[i] //nincs pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
